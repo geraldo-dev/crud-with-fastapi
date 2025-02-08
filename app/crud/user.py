9daa96ec7f240bcaa231a8576bffdd2981acfc14
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-08 14:46:41 -0300

    function delete user

--- app/crud/user.py
+++ app/crud/user.py
@@ -19,3 +19,12 @@ def create_user(db: Session, user: UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+        return True
+    return False
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserResponse
-from app.crud.user import get_user, get_users, create_user
+from app.crud.user import get_user, get_users, create_user, delete_user
 from app.database import get_db
 
 
@@ -13,7 +13,7 @@ def list_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return get_users(db=db, skip=skip, limit=limit)
 
 
-@router.get('/:id', response_model=UserResponse)
+@router.get('/{user_id}', response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = get_user(user_id=user_id, db=db)
     if not user:
@@ -24,3 +24,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 @router.post('/', response_model=UserResponse)
 def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db=db, user=user)
+
+
+@router.delete('/{user_id}')
+def destroy(user_id: int, db: Session = Depends(get_db)):
+    success = delete_user(db=db, user_id=user_id)
+    if not success:
+        raise HTTPException(status_code=404, detail='User not foud')
+    return {'message': 'User deleted'}
