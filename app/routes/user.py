4359c443641849aff9e91a4ac0475d95216dcfe8
geraldo ferreira <geraldosousadev@gmail.com>
2025-02-09 14:26:16 -0300

    remove folder test

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from http import HTTPStatus
 from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserResponse, UserUpdatePartial
 from app.crud.user import get_user, get_users, create_user, delete_user, update_user_partial, check_email
@@ -8,12 +9,12 @@ from app.models.user import User
 router: APIRouter = APIRouter()
 
 
-@router.get('/', response_model=list[UserResponse])
+@router.get('/', status_code=HTTPStatus.OK, response_model=list[UserResponse])
 def list_users(skip: int = 0, limit: int = 10, db: Session = Depends(dependency=get_db)) -> list[User]:
     return get_users(db=db, skip=skip, limit=limit)
 
 
-@router.get('/{user_id}', response_model=UserResponse)
+@router.get('/{user_id}', status_code=HTTPStatus.OK,  response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(dependency=get_db)) -> User:
     user: User | None = get_user(user_id=user_id, db=db)
     if not user:
@@ -21,7 +22,7 @@ def read_user(user_id: int, db: Session = Depends(dependency=get_db)) -> User:
     return user
 
 
-@router.post('/', response_model=UserResponse, summary='criar usuario')
+@router.post('/', status_code=HTTPStatus.CREATED,  response_model=UserResponse, summary='criar usuario')
 def create_new_user(user: UserCreate, db: Session = Depends(dependency=get_db)) -> User:
     existing_user: User | bool = check_email(db, user.email)
     if existing_user:
@@ -30,7 +31,7 @@ def create_new_user(user: UserCreate, db: Session = Depends(dependency=get_db)) 
     return create_user(db=db, user=user)
 
 
-@router.delete('/{user_id}')
+@router.delete('/{user_id}', status_code=HTTPStatus.OK)
 def destroy(user_id: int, db: Session = Depends(dependency=get_db)) -> dict[str, str]:
     success: bool = delete_user(db=db, user_id=user_id)
     if not success:
@@ -38,7 +39,7 @@ def destroy(user_id: int, db: Session = Depends(dependency=get_db)) -> dict[str,
     return {'message': 'User deleted'}
 
 
-@router.put('/{user_id}', response_model=UserResponse)
+@router.put('/{user_id}', status_code=HTTPStatus.OK,  response_model=UserResponse)
 def update_user_field(user_id: int, updated_data: UserUpdatePartial, db: Session = Depends(dependency=get_db)):
 
     user: User | bool = get_user(db, user_id)
